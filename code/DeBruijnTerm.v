15bffd3db9f499abc0aa30ab3bf1726acf735f10
Quentin Fiard <quentin.fiard@polytechnique.org>
2012-12-16 21:17:14 +0100

    Code update

--- code/DeBruijnTerm.v
+++ code/DeBruijnTerm.v
@@ -5,46 +5,22 @@ Module DeBruijnTerms.
 
 Section Declaration.
 
+(*
+ * Définition inductive des termes de De Bruijn (DBT)
+ *)
 Inductive DBT:Type :=
 | Var : nat -> DBT
 | Fun : DBT -> DBT
 | Appl : DBT -> DBT -> DBT.
 
 (*
-Definition 
-
-Definition free_variables (t:DBT) : set nat.
-Proof.
-  (* Cas d'une variable *)
-  elim:t.
-  move => x.
-  move/(_ nat):empty_set => s.
-  move:set_add => h.
-  move/(_ nat):h => h.
-  apply:h.
-  move:nat_unicity => h.
-  done.
-  done.
-  done.
-  
-  (* Cas d'une fonction *)
-  move => t free_t.
-  move:set_remove => h.
-  move/(_ nat):h => h.
-  apply:h.
-  move:nat_unicity => h.
-  done.
-  apply:0.
-  done.
-
-  (* Cas d'une application *)
-
-  done.
-Defined.
-*)
-
+ * Propriété que doit vérifier une fonction de deux variables pour être héréditaire par rapport à sa première variable
+ *)
 Definition heredity_n_k (f:nat -> nat -> Prop) : Prop := forall n k:nat, f n k -> f (n+1) k.
 
+(*
+ * Un lemme très simple, croissance de la relation inférieur
+ *)
 Lemma lt_succ : forall x y:nat, x<y -> x<y+1.
 Proof.
   move => x y.
@@ -52,74 +28,153 @@ Proof.
   done.
 Qed.
 
-Definition n_k_free (t:DBT) : { f:(nat->nat->bool) | heredity_n_k f}.
+(*
+ * n_k_free construit, étant donné un terme t, une fonction qui pour tout n, k, renvoie
+ * true si et seulement si les variables libres de t sont < n, sachant que toutes les
+ * variables < k sont liées.
+ *
+ * On prouve en parallèle l'hérédité de la fonction renvoyé, tel que défini précédemment
+ *)
+Definition n_k_free (t:DBT) : nat->nat->bool.
 Proof.
   elim:t.
 
   (* Variable *)
-  move => x.
-  exists (fun n:nat => (fun k:nat => ((x<k)||(x<n)))).
-  rewrite/heredity_n_k.
-  move => n k.
+  move => x n k.
+  apply:((x<k)||(x<n)).
+
+  (* Fonction *)
+  move => t ih n k.
+  apply:(ih n (k+1)).
+
+  (* Application *)
+  move => t iht u ihu n k.
+  apply:((iht n k)&&(ihu n k)).
+Defined.
+
+Theorem n_k_free_heredity : forall t:DBT, heredity_n_k (n_k_free t).
+Proof.
+  move => t.
+  rewrite /n_k_free.
+  rewrite /heredity_n_k.
+  elim t.
+
+  (* Variable *)
+  simpl.
+  move => n p q.
   move/orP.
-  case.
-  move => -> //=.
-  move => h.
-  have h2:x<n+1.
-  move/(_ x n):lt_succ => h1.
-  apply:h1.
+  move => h1.
+  case:h1 => h1.
+  move:h1 => -> //=.
+  move/(_ n p):lt_succ => h2.
+  have h3 : n<p+1.
+  apply:h2.
   done.
-  move:h2 => ->.
+  move:h3 => ->.
   rewrite orbT.
   done.
 
   (* Fonction *)
-  move => t ih.
-  case:ih => f p.
-  exists (fun n => (fun k => (f n (k+1)))).
-  move:p.
-  rewrite/heredity_n_k.
-  move => ih n k h.
+  simpl.
+  move => d ih n k.
   move/(_ n (k+1)):ih => ih.
   apply:ih.
-  done.
 
   (* Application *)
-  move => t iht u ihu.
-  case:iht => ft pt.
-  case:ihu => fu pu.
-
-  exists (fun n => (fun k => ((ft n k)||(fu n k)))).
-
-  move:pt pu.
-  rewrite/heredity_n_k.
-  move => iht ihu n k h.
-  move/(_ n k): iht => iht.
-  move/(_ n k): ihu => ihu.
-
-  move/orP:h => h.
-
-  case:h => h.
-
-  have h1: ft (n+1) k.
-  apply:iht.
+  simpl.
+  move => u ihu v ihv n k h.
+  move/andP:h => h.
+  case:h => h1 h2.
+  move/(_ n k):ihu => ihu.
+  move/(_ n k):ihv => ihv.
+  have left:DBT_rec (fun _ : DBT => nat -> nat -> bool)
+     (fun x n0 k0 : nat => (x < k0) || (x < n0))
+     (fun (_ : DBT) (ih : nat -> nat -> bool) (n0 k0 : nat) => ih n0 (k0 + 1))
+     (fun (_ : DBT) (iht : nat -> nat -> bool) (_ : DBT)
+        (ihu0 : nat -> nat -> bool) (n0 k0 : nat) => 
+      iht n0 k0 && ihu0 n0 k0) u (n + 1) k.
+  apply:ihu.
+  apply:h1.
+  have right:DBT_rec (fun _ : DBT => nat -> nat -> bool)
+     (fun x n0 k0 : nat => (x < k0) || (x < n0))
+     (fun (_ : DBT) (ih : nat -> nat -> bool) (n0 k0 : nat) => ih n0 (k0 + 1))
+     (fun (_ : DBT) (iht : nat -> nat -> bool) (_ : DBT)
+        (ihu0 : nat -> nat -> bool) (n0 k0 : nat) => 
+      iht n0 k0 && ihu0 n0 k0) v (n + 1) k.
+  apply:ihv.
+  apply:h2.
+  move:left => ->.
+  move:right => ->.
   done.
+Qed.
 
-  move:h1 => -> //=.
+Theorem n_k_free_heredity2 : forall t:DBT, forall n k:nat, n_k_free t n k -> n_k_free t n (k+1).
+Proof.
+  move => t.
+  rewrite /n_k_free.
+  elim:t.
 
-  have h1 : fu (n+1) k.
-  apply:ihu => //.
-
+  (* Variable *)
+  move => x n k.
+  simpl.
+  move/orP.
+  move => h1.
+  case:h1 => h1.
+  move/(_ x k):lt_succ => h2.
+  have h3:x<k+1.
+  apply:h2.
+  done.
+  move:h3 => -> //=.
   move:h1 => ->.
   rewrite orbT.
   done.
-Defined.
 
-Definition n_free (t:DBT) : { f:nat -> bool | forall n:nat, forall t:DBT, f n -> f (n+1) }.
-Proof.
-  move:n_k_free => h.
-  case:h.
+  (* Function *)
+  simpl.
+  move => t ih n k.
+  move/(_ n (k+1)):ih => ih.
+  apply:ih.
+
+  (* Application *)
+  simpl.
+  move => t iht u ihu n k.
+  move/(_ n k):iht => iht.
+  move/(_ n k):ihu => ihu.
+  move/andP => h1.
+  case:h1 => h1 h2.
+  have h3:DBT_rec (fun _ : DBT => nat -> nat -> bool)
+     (fun x n0 k0 : nat => (x < k0) || (x < n0))
+     (fun (_ : DBT) (ih : nat -> nat -> bool) (n0 k0 : nat) => ih n0 (k0 + 1))
+     (fun (_ : DBT) (iht0 : nat -> nat -> bool) (_ : DBT)
+        (ihu0 : nat -> nat -> bool) (n0 k0 : nat) => 
+      iht0 n0 k0 && ihu0 n0 k0) t n (k + 1).
+  apply:iht.
   done.
+  have h4:DBT_rec (fun _ : DBT => nat -> nat -> bool)
+     (fun x n0 k0 : nat => (x < k0) || (x < n0))
+     (fun (_ : DBT) (ih : nat -> nat -> bool) (n0 k0 : nat) => ih n0 (k0 + 1))
+     (fun (_ : DBT) (iht0 : nat -> nat -> bool) (_ : DBT)
+        (ihu0 : nat -> nat -> bool) (n0 k0 : nat) => 
+      iht0 n0 k0 && ihu0 n0 k0) u n (k + 1).
+  apply:ihu.
+  done.
+  rewrite h3.
+  rewrite h4.
+  done.
+Qed.
+
+(*
+ * n_free t n renvoie true si et seulement si l'ensemble des variables libres de t
+ * sont inférieurs strictement à n.
+ *
+ * Là encore, on démontre en parallèle l'hérédité de cette propriété
+ *)
+Definition n_free (t:DBT) : nat -> bool.
+Proof.
+  move => n.
+  move/(_ t n O):n_k_free => h.
+  done.
+  (*
   move => n_k_free.
   rewrite/heredity_n_k.
   move => p.
@@ -128,17 +183,30 @@ Proof.
   move => n.
   move/(_ n 0):p => p.
   done.
+*)
 Defined.
 
-Fixpoint closed (t:DBT) : bool.
+Theorem n_free_heredity : forall t:DBT, forall n:nat, n_free t n -> n_free t (n+1).
 Proof.
-  move:n_free => h.
-  case:h.
+  move => t n.
+  rewrite /n_free.
+  move/(_ t n 0):n_k_free_heredity => h1.
   done.
-  move => n p.
-  apply:n 0.
+Qed.
+
+(*
+ * Un terme t est clos si et seulement si n_free t 0 = true
+ *)
+Definition closed : DBT -> bool.
+Proof.
+  move => t.
+  move/(_ t 0):n_free => b.
+  done.
 Defined.
 
+(*
+ * Quelques tests élémentaires
+ *)
 Definition t1:DBT := Var 0.
 Definition t2:DBT := Fun (Var 0).
 Definition t3:DBT := Fun (Var 1).
@@ -147,6 +215,10 @@ Compute closed t1.
 Compute closed t2.
 Compute closed t3.
 
+(*
+(*
+ * Quelques fonctions utiles dans le cas d'une fonction.
+ *)
 Definition is_function (f:DBT) : bool :=
   match f with
     | Fun t => true
@@ -193,8 +265,12 @@ Proof.
   apply : (hr2 n).
   apply : (b && c).
 Qed.
+*)
 
-(* add_n_to_free_k t n k  adds a constant value n to all free variables >= k in t *)
+(*
+ * add_n_to_free_k t n k  ajoute une valeur constante n à toutes les variables libres
+ * de t, sachant que toutes les variables < k sont liées 
+ *)
 Definition add_n_to_free_k : DBT -> nat -> nat -> DBT.
 Proof.
   move => t.
@@ -214,7 +290,9 @@ Proof.
   apply: Appl (ih1 n k) (ih2 n k).
 Defined.
 
-(* add_n_to_free adds a constant value n to all the free variables of the argument DBT *)
+(*
+ * add_n_to_free t n ajoute n à toutes les variables libres de t
+ *)
 Definition add_n_to_free : DBT -> nat -> DBT.
 Proof.
   move => t n.
@@ -224,6 +302,9 @@ Defined.
 Compute (add_n_to_free t2 10).
 Compute (add_n_to_free t3 10).
 
+(*
+ * shift incrémente toutes les variables libres d'un DBT
+ *)
 Definition shift : DBT -> DBT.
 Proof.
   move => t.
@@ -232,6 +313,10 @@ Defined.
 
 Compute (shift t3).
 
+(*
+ * Substitution simple dans un DBT. Étant donné une variable libre et un terme, on substitue ce terme
+ * à chaque occurence de la variable libre.
+ *)
 Definition substitute_one : DBT -> nat -> DBT -> DBT.
 Proof.
   move => t.
@@ -252,10 +337,135 @@ Proof.
   apply:(Appl (ih1 n u) (ih2 n u)).
 Defined.
 
-Theorem substitute_in_closed : forall f:{f:DBT | is_function f}, forall u:DBT, closed (element f) ->  substitution f u = (element f).
+Lemma leq_neq : forall x n:nat, x<n -> (n==x)=false.
+Admitted.
+
+Lemma leq_1 : forall x n:nat, x<0+1 -> x<n+1.
+Admitted.
+
+Lemma n_k_free_arg1 : forall  t:DBT, forall n k:nat, n_k_free (Fun t) n k -> n_k_free t (n+1) (k+1).
 Proof.
-  move => f u.
-  rewrite/element.
+  move => t.
+  elim:t.
+
+  (* Variable *)
+  simpl.
+  move => x n k.
+  move/orP.
+  case => h1.
+  
+  rewrite h1.
+  done.
+  move/(_ x n):lt_succ => h2.
+  have h3:x<n+1.
+  apply:h2.
+  done.
+  rewrite h3.
+  rewrite orbT.
+  done.
+
+  (* Fonction *)
+  rewrite /n_k_free.
+  simpl.
+
+  move => t ih n k.
+  move/(_ n (k+1)):ih => ih.
+  apply:ih.
+
+  (* Application *)
+  move => t iht u ihu n k.
+  move/(_ n k):iht => iht.
+  move/(_ n k):ihu => ihu.
+  simpl.
+  move/andP.
+  case => h1 h2.
+  have h3:n_k_free t (n + 1) (k + 1).
+  apply:iht.
+  done.
+  have h4:n_k_free u (n + 1) (k + 1).
+  apply:ihu.
+  done.
+  rewrite h3.
+  rewrite h4.
+  done.
+Qed.
+
+Lemma n_free_arg1 : forall t:DBT, forall n k:nat, n_free (Fun t) n -> n_free t (n+1).
+Admitted.
+
+Lemma n_free_arg2 : forall t1 t2:DBT, forall n:nat, n_free (Appl t1 t2) n -> (n_free t1 n)/\(n_free t2 n).
+Admitted.
+
+Theorem substitute_with_free_variables_above : forall t u:DBT, forall n:nat, n_free t n -> substitute_one t n u = t.
+Proof.
+  move => t.
+  elim:t.
+
+  (* Variable *)
+  rewrite /n_free.
+  rewrite /n_k_free.
+  rewrite /substitute_one.
+  simpl.
+  move => x u n h1.
+  move/(_ x n):leq_neq => h2.
+  have h3:(n==x)=false.
+  apply:h2.
+  done.
+  rewrite h3.
+  done.
+
+  (* Fonction *)
+  simpl.
+  move => t ih u n.
+  move/(_ t n):n_free_arg1 => h1.
+  move/(_ (shift u) (n+1)):ih => ih.
+  move => h3.
+  have h4:substitute_one t (n + 1) (shift u) = t.
+  apply:ih.
+  apply:h1.
+  done.
+  done.
+  rewrite h4.
+  done.
+
+  (* Application *)
+  simpl.
+  move => t1 iht1 t2 iht2 u n h1.
+  move/(_ u n):iht1 => iht1.
+  move/(_ u n):iht2 => iht2.
+  move/(_ t1 t2 n):n_free_arg2 => h2.
+  have h3:n_free t1 n /\ n_free t2 n.
+  apply:h2.
+  done.
+  have h4: substitute_one t1 n u = t1.
+  apply:iht1.
+  case:h3 =>//.
+  have h5: substitute_one t2 n u = t2.
+  apply:iht2.
+  case:h3 =>//.
+  rewrite h4.
+  rewrite h5.
+  done.
+Qed.
+
+Theorem substitute_in_closed : forall t u:DBT, forall n:nat, closed t ->  substitute_one t n u = t.
+Proof.
+  move => t u n.
+  rewrite /closed.
+  move => h1.
+  have h2: n_free t n.
+  elim:n.
+  done.
+
+  move =>  n.
+  move/(_ t n):n_free_heredity => h2.
+  move/(_ n):addn1 => h3.
+  rewrite -h3.
+  done.
+
+  move/(_ t u n):substitute_with_free_variables_above => h3.
+  apply:h3.
+  done.
 Qed.
 
 Definition n_free_list : list DBT -> nat -> bool.
@@ -269,8 +479,7 @@ Proof.
   have test:bool.
 
   move/(_ t):n_free => h.
-  case:h => f _.
-  apply:f.
+  apply:h.
   apply:n.
 
   apply:(test&&ih).
@@ -289,9 +498,34 @@ Proof.
   apply:(t::ih).
 Defined.
 
-Definition substitute : DBT -> list (nat*DBT) -> DBT.
+Definition substitute_in_nat_DBT_list : list (nat*DBT) -> nat -> DBT -> list (nat*DBT).
 Proof.
-  move => t l.
+  move => l.
+  elim:l.
+  
+  (* Liste vide *)
+  move => n u.
+  apply:nil.
+
+  (* Suite *)
+  move => p s ih n u.
+  case:p => x t.
+  move/(_ n u):ih => ih.
+  apply:((x,substitute_one t n u)::ih).
+Defined.
+
+Fixpoint reverse_aux (A:Type) (l1:list A) (l2:list A) : list A :=
+  match l1 with
+    | nil => l2
+    | x::s => reverse_aux A s (x::l2)
+  end.
+
+Definition reverse  (A:Type) (l1:list A) : list A := reverse_aux A l1 nil.
+
+Compute (reverse nat (1::2::3::nil)).
+
+Definition substitute_aux (t:DBT) (l:list (nat*DBT)) : DBT.
+Proof.
   elim:l.
 
   (* Empty list *)
@@ -305,33 +539,28 @@ Proof.
   apply:substitute_one ih i ui.
 Defined.
 
+(*
+ * On commence par inverser la liste de substitution, puis on effectue les substitutions en tant que telles.
+ *)
+
+Definition substitute (t:DBT) (l:list (nat*DBT)) : DBT.
+Proof.
+  have l2:list (nat*DBT).
+  apply:(reverse (nat*DBT) l).
+  apply:(substitute_aux t l2).
+Defined.
+
 Theorem substitute_nil: forall t:DBT, substitute t nil = t.
 Proof.
-  move => t.
-  simpl.
-  done.
+  done. (* La propriété est prouvé automatiquement par Coq *)
 Qed.
 
-<<<<<<< HEAD
 Definition pair_left: (nat*DBT) -> nat.
-=======
-Definition substitution : {f:DBT | is_function f} -> DBT -> DBT.
-Admitted.
-(*
->>>>>>> origin/master
 Proof.
   move => p.
-<<<<<<< HEAD
   case:p => i t.
-  apply:i.
+  apply : i.
 Defined.
-=======
-  move/(_ f):function_arg => t.
-
-  move/(_ t 0 u):replace => h.
-  apply:h.
-Defined.*)
->>>>>>> origin/master
 
 Definition pair_right: (nat*DBT) -> DBT.
 Proof.
@@ -342,24 +571,12 @@ Defined.
 
 Theorem substitute_one_elem : forall t x:DBT, forall i:nat, substitute t ((i,x)::nil) = substitute_one t i x.
 Proof.
-  move => t x i.
-  simpl.
-  done.
+  done. (* La propriété est prouvé automatiquement par Coq *)
 Defined.
 
 Theorem substitute_rec : forall t x:DBT, forall s:(list (nat*DBT)), forall i:nat, n_free_list (list_from_list_of_pairs s) i -> substitute t ((i,x)::s) = substitute_one (substitute t s) i x.
 Admitted.
 
-(*
-Fixpoint equals (a:DBT) (b:DBT) : Prop :=
-match a,b with
-|_, _ => False
-end.
-  move=> a b.
-  elim a.
-  elim b.
-
-Defined.*)
 Inductive reduces_in_one_step : DBT -> DBT -> Prop :=
 | Lambda_reduction : forall t u, reduces_in_one_step (Appl (Fun t) u) (substitute_one t 0 u)
 | Rep_Lambda : forall t u, reduces_in_one_step t u ->reduces_in_one_step (Fun t) (Fun u)
@@ -495,6 +712,7 @@ Proof.
   apply:((Push ihu)::iht).
 Defined.
 
+(*
 Definition execute :state->state.
 Proof.
   move => s.
@@ -548,3 +766,5 @@ move/(_ c e s  ).
 move=>st.
 rewrite/(_ state->u).
 st.
+
+*)
